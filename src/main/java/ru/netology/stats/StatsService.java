a852380fc203a4f06fb6ceeed7772bd9eadc1b7d
Margarita Korolkova <mkorolkova311@gmail.com>
2020-11-21 22:42:59 +0300

    Initial commit

---
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -0,0 +1,72 @@
+package ru.netology.stats;
+
+public class StatsService {
+    public int sum(int[] purchases) {
+        int result = 0;
+
+        for (int purchase : purchases) {
+            result += purchase;
+
+        }
+
+
+        return result;
+
+    }
+
+    public int average(int[] purchases) {
+
+        return sum(purchases) / purchases.length;
+
+    }
+
+    public int getMonthMaxSales(int[] purchases) {
+        int max = getMax(purchases);
+        int monthNumber = 0;
+        int monthWihMax = 0;
+        for (int purchase : purchases) {
+            monthNumber++;
+            if (purchase == max) {
+                monthWihMax = monthNumber;
+
+            }
+        }
+        return monthWihMax;
+
+    }
+
+    public int getMax(int[] purchases) {
+        int max = purchases[0];
+        for (int purchase : purchases) {
+            if (purchase > max) {
+                max = purchase;
+            }
+        }
+        return max;
+    }
+
+
+    public int getMin(int[] purchases) {
+        int min = purchases[0];
+        for (int purchase : purchases) {
+            if (purchase < min) {
+                min = purchase;
+            }
+        }
+        return min;
+    }
+
+    public int getMonthMinSales(int[] purchases) {
+        int min = getMin(purchases);
+        int monthNumber = 0;
+        int monthWihMin = 0;
+        for (int purchase : purchases) {
+            monthNumber++;
+            if (purchase == min) {
+                monthWihMin = monthNumber;
+
+            }
+        }
+        return monthWihMin;
+    }
+}
---
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -0,0 +1,51 @@
+package ru.netology.stats;
+
+import org.junit.jupiter.api.Test;
+
+import java.security.Provider;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatsServiceTest {
+    private int[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+    private StatsService service = new StatsService();
+
+    @Test
+    void shouldAmountAllSales() {
+
+        int expected = 180;
+        int actual = service.sum(purchases);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldAveregeSale() {
+
+        int expected = 15;
+        int actual = service.average(purchases);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldMonthOfMaxSales() {
+        int expected = 8;
+        int actual = service.getMonthMaxSales(purchases);
+
+        assertEquals(expected, actual);
+
+
+    }
+
+    @Test
+    void shouldMonthfMinSales() {
+        int expected = 9;
+        int actual = service.getMonthMinSales(purchases);
+
+        assertEquals(expected, actual);
+
+
+    }
+}
+
